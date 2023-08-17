f91ec78ad3c7fa4d971cec78d353013a8daaf471
goghrf <daynnite9924@gmail.com>
2023-08-17 16:05:40 +0900

    MemberRepository 인터페이스 생성

---
+++ zerotoone/src/main/java/org/zerock/zerotoone/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.zerotoone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.zerotoone.domain.Member;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+}
