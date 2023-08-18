6b471dbf63bf28783c4beab910c4982ee6ba1a67
goghrf <daynnite9924@gmail.com>
2023-08-18 18:53:06 +0900

    프로젝트 삭제

--- zerotoone/src/main/java/org/zerock/zerotoone/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.zerock.zerotoone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.zerotoone.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-
-}
