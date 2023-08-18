25231c9ae2a7ba3ae83fb4c554dcee80b9e41115
goghrf <daynnite9924@gmail.com>
2023-08-18 18:59:24 +0900

    프로젝트 생성

---
+++ zerotoone/src/main/java/org/zerock/zerotoone/ZerotooneApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.zerotoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerotooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerotooneApplication.class, args);
+	}
+
+}
---
+++ zerotoone/src/test/java/org/zerock/zerotoone/ZerotooneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.zerotoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerotooneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
