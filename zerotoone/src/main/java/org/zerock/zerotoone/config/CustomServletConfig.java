21fc971cab3da42c37f2b26132d9c7528fb10866
goghrf <daynnite9924@gmail.com>
2023-08-19 01:38:31 +0900

    CustomServletConfig 클래스 생성

---
+++ zerotoone/src/main/java/org/zerock/zerotoone/config/CustomServletConfig.java
@@ -0,0 +1,6 @@
+package org.zerock.zerotoone.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CustomServletConfig implements WebMvcConfigurer {
+}
