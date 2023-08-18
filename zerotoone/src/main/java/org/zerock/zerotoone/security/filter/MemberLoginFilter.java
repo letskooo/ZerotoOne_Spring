10715b2e97d4dcf63e77e444e2bc2de82bb943bd
goghrf <daynnite9924@gmail.com>
2023-08-19 05:24:29 +0900

    JSON 로그인 요청 정보 반환 로직 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/security/filter/MemberLoginFilter.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/security/filter/MemberLoginFilter.java
@@ -1,6 +1,8 @@
 package org.zerock.zerotoone.security.filter;
 
+import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
@@ -9,6 +11,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
 
 @Log4j2
 public class MemberLoginFilter extends AbstractAuthenticationProcessingFilter {
@@ -23,7 +28,35 @@ public class MemberLoginFilter extends AbstractAuthenticationProcessingFilter {
 
         log.info("-----------------MemberLoginFilter----------------");
 
-        return null;
+        if(request.getMethod().equalsIgnoreCase("GET")){
+            log.info("GET METHOD NOT SUPPORT");
+            return null;
+        }
 
+        Map<String, String > jsonData = parseRequestJSON(request);
+
+        log.info(jsonData);
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken
+                = new UsernamePasswordAuthenticationToken(
+                        jsonData.get("username"),
+                        jsonData.get("password"));
+
+        // AuthenticationManager에게 인증 위임
+        return getAuthenticationManager().authenticate(usernamePasswordAuthenticationToken);
+
+    }
+
+    private Map<String, String> parseRequestJSON(HttpServletRequest request){
+
+        try(Reader reader = new InputStreamReader(request.getInputStream())){
+
+            Gson gson = new Gson();
+
+            return gson.fromJson(reader, Map.class);
+        } catch(Exception e){
+            log.error(e.getMessage());
+        }
+        return null;
     }
 }
