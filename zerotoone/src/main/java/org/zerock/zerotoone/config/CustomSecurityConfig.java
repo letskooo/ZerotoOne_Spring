456abaeb3984bed86ddbfcf0911c7498b4903f54
goghrf <daynnite9924@gmail.com>
2023-08-19 05:04:14 +0900

    MemberLoginFilter 설정 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/config/CustomSecurityConfig.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/config/CustomSecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +15,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.zerock.zerotoone.security.MemberDetailsService;
+import org.zerock.zerotoone.security.filter.MemberLoginFilter;
 
 @Configuration
 @Log4j2
@@ -21,6 +26,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
+    private final MemberDetailsService memberDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -28,6 +35,25 @@ public class CustomSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+
+        // AuthenticationManager 설정
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(
+                AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(memberDetailsService).passwordEncoder(passwordEncoder());
+
+        // AuthenticationManger 생성
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        // authenticationManager로 설정
+        http.authenticationManager(authenticationManager);
+
+        // MemberLoginFilter
+        // 로그인 처리 경로 설정. 사용자가 이 경로로 로그인 요청을 보낼 때 실행
+        MemberLoginFilter memberLoginFilter = new MemberLoginFilter("/login");
+        memberLoginFilter.setAuthenticationManager(authenticationManager);
+
+        // MemberLoginFilter 위치 설정
+        http.addFilterBefore(memberLoginFilter, UsernamePasswordAuthenticationFilter.class);
 
         log.info("---------configure-----------");
 
