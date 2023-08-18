c9a0d7c3cb0b211f6d482907790ad658103166b7
goghrf <daynnite9924@gmail.com>
2023-08-19 04:51:38 +0900

    MemberController 생성

---
+++ zerotoone/src/main/java/org/zerock/zerotoone/controller/MemberController.java
@@ -0,0 +1,23 @@
+package org.zerock.zerotoone.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.zerotoone.domain.Member;
+import org.zerock.zerotoone.dto.MemberLoginDTO;
+import org.zerock.zerotoone.security.MemberDetailsService;
+
+@RestController
+@RequestMapping("/member")
+@Log4j2
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberDetailsService memberDetailsService;
+
+
+
+
+}
