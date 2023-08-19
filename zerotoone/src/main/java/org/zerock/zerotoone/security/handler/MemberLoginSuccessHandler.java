693dc9b828a82714415125ddc9d4f31e44ef1d2e
goghrf <daynnite9924@gmail.com>
2023-08-19 15:46:41 +0900

    검증된 사용자 토큰 생성 로직 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/security/handler/MemberLoginSuccessHandler.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/security/handler/MemberLoginSuccessHandler.java
@@ -1,18 +1,24 @@
 package org.zerock.zerotoone.security.handler;
 
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.zerock.zerotoone.Util.JWTUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
 public class MemberLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -20,6 +26,26 @@ public class MemberLoginSuccessHandler implements AuthenticationSuccessHandler {
 
         log.info("-------------Login Success Handler--------------");
 
-    }
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
+        log.info(authentication);
+        log.info(authentication.getName()); // 클라이언트에서 보낸 username
+
+        Map<String, Object> claim = Map.of("username", authentication.getName());
+        // Access Token 유효기간 1일
+        String accessToken = jwtUtil.generateToken(claim, 1);
+        // Refresh Token 유효 기간 30일
+        String refreshToken = jwtUtil.generateToken(claim, 30);
+
+        Gson gson = new Gson();
+
+        Map<String, String> keyMap = Map.of(
+                "accessToken", accessToken,
+                "refreshToken", refreshToken);
+
+        String jsonStr = gson.toJson(keyMap);
+
+        response.getWriter().println(jsonStr);
+
+    }
 }
