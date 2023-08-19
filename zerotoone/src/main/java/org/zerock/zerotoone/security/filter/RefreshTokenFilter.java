41c116452dd8483e98ee68a6eae9961bebfc3956
goghrf <daynnite9924@gmail.com>
2023-08-19 21:34:39 +0900

    RefreshToken 만료 시 재발급 로직 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/security/filter/RefreshTokenFilter.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/security/filter/RefreshTokenFilter.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Map;
 
 @Log4j2
@@ -63,6 +65,36 @@ public class RefreshTokenFilter extends OncePerRequestFilter {
         try{
             refreshClaims = checkRefreshToken(refreshToken);
             log.info(refreshClaims);
+
+            Integer exp = (Integer) refreshClaims.get("exp");
+            // 만료 시간
+            Date expTime = new Date(Instant.ofEpochMilli(exp).toEpochMilli() * 1000);
+            // 현재 시간
+            Date current = new Date(System.currentTimeMillis());
+            long gapTime = (expTime.getTime() - current.getTime());
+
+            log.info("------------------------");
+            log.info("current: " + current);
+            log.info("expTime: " + expTime);
+            log.info("gap: " + gapTime);
+
+            String username = (String)refreshClaims.get("username");
+
+            // Access Token 재발급
+            String accessTokenValue = jwtUtil.generateToken(Map.of("username", username), 1);
+            String refreshTokenValue = tokens.get("refreshToken");
+
+            // RefreshToken 재발급 기간을 만료일로부터 3일 전으로 지정
+            if (gapTime < (1000 * 60 * 60 * 24 * 3)){
+                log.info("new Refresh Token required....");
+                refreshTokenValue = jwtUtil.generateToken(Map.of("username", username), 30);
+            }
+
+            log.info("Refresh Token Result .......");
+            log.info("AccessToken: " + accessTokenValue);
+            log.info("RefreshToken; " + refreshTokenValue);
+
+
         } catch (RefreshTokenException refreshTokenException){
             refreshTokenException.sendResponseError(response);
             return;
