0c8bac8d57dab29a17cdc403279c5dafcf760b88
goghrf <daynnite9924@gmail.com>
2023-08-19 20:55:59 +0900

    RefreshTokenFilter 생성

---
+++ zerotoone/src/main/java/org/zerock/zerotoone/security/filter/RefreshTokenFilter.java
@@ -0,0 +1,35 @@
+package org.zerock.zerotoone.security.filter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerock.zerotoone.Util.JWTUtil;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+@RequiredArgsConstructor
+public class RefreshTokenFilter extends OncePerRequestFilter {
+
+    private final String refreshPath;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        if(!path.equals(refreshPath)){
+            log.info("skip refresh token filter........");
+            filterChain.doFilter(request, response);
+            return;
+        }
+        log.info("Refresh Token Filter...run......");
+
+    }
+}
