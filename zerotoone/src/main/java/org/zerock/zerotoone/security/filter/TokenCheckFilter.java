ce95cd40a54747c273a3893b42d5112cb671eee6
goghrf <daynnite9924@gmail.com>
2023-08-19 20:13:21 +0900

    AccessToken 예외 처리 메소드 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/security/filter/TokenCheckFilter.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/security/filter/TokenCheckFilter.java
@@ -1,9 +1,13 @@
 package org.zerock.zerotoone.security.filter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.zerock.zerotoone.Util.JWTUtil;
+import org.zerock.zerotoone.security.exception.AccessTokenException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.rmi.server.ServerCloneException;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -37,4 +42,39 @@ public class TokenCheckFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
 
     }
+
+    private Map<String, Object> validateAccessToken(HttpServletRequest request) throws AccessTokenException {
+
+        String headStr = request.getHeader("Authorization");
+
+        if (headStr == null || headStr.length() < 8){
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.UNACCEPT);
+        }
+
+        String tokenType = headStr.substring(0, 6);
+        String tokenStr = headStr.substring(7);
+
+        if(tokenType.equalsIgnoreCase("Bearer") == false){
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADTYPE);
+        }
+
+        try {
+            Map<String, Object> values = jwtUtil.validateToken(tokenStr);
+            return values;
+        } catch (MalformedJwtException malformedJwtException){
+            log.error("-------MalformedJwtException----------");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.MALFORM);
+
+        } catch(SignatureException signatureException) {
+            log.error("-------SignatureException-------------");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADSIGN);
+
+        } catch(ExpiredJwtException expiredJwtException){
+            log.error("---------ExpiredJwtException-----------");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.EXPIRED);
+
+        }
+
+    }
+
 }
