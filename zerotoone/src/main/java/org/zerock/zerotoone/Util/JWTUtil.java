b9a8fdb8e1738b8e0cb347fa30af64edcc76845f
goghrf <daynnite9924@gmail.com>
2023-08-19 15:17:26 +0900

    토큰 생성 테스트 코드 추가

--- zerotoone/src/main/java/org/zerock/zerotoone/Util/JWTUtil.java
+++ zerotoone/src/main/java/org/zerock/zerotoone/Util/JWTUtil.java
@@ -1,23 +1,49 @@
 package org.zerock.zerotoone.Util;
 
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 @Log4j2
 public class JWTUtil {
 
-    @Value("${org.zerock.jwt.secret")
+    @Value("${org.zerock.jwt.secret}")
     private String key;
 
     public String generateToken(Map<String, Object> valueMap, int days){
         log.info("generateKey.." + key);
-        return null;
+
+        // Header
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        // payload
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.putAll(valueMap);
+
+        int time = (1) * days;
+
+        String jwtStr = Jwts.builder()
+                .setHeader(headers)
+                .setClaims(payloads)
+                .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(time).toInstant()))
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+
+        return jwtStr;
     }
+
     public Map<String, Object> validateToken(String token) throws JwtException {
 
         Map<String, Object> claim = null;
