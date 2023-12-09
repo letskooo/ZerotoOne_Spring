2706648dbfc4351acd1c40182570366300339ade
imnoans <sy1004da@gmail.com>
2023-12-09 16:03:49 +0900

    before_pull

--- studypartner/src/main/java/pm2_5/studypartner/service/MultipleService.java
+++ studypartner/src/main/java/pm2_5/studypartner/service/MultipleService.java
@@ -1,4 +1,16 @@
 package pm2_5.studypartner.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pm2_5.studypartner.util.OpenaiUtil;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
 public class MultipleService {
+
+    private final OpenaiUtil openaiUtil;
 }
